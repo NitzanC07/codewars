4acdb145888ff1a47506a53c5ec16e0ac3dd7e00
Nitzan Cohen <nitzancohen@hotmail.com>
2022-12-28 00:25:16 +0200

    New Exercise format phone number

--- codeWars/Level_6/FormatPhone.js
+++ codeWars/Level_6/FormatPhone.js
@@ -1,17 +1,36 @@
 function formatNumber(number, template) {
-    const len = (String(number)).length;
-    let phoneNumber = "";
-    let n = len-1;
-    for(let i = 0; i < template.length; i++) {
-        if(template[i] === "#") {
-            phoneNumber += String(Math.floor(number / 10**n) % 10);
-            n -= 1;
-        } else {
-            phoneNumber = phoneNumber + template[i];
-        }
+    let counter = 0;
+    for (let c = 0; c < template.length; c++) {
+        template[c] === "#" ? counter += 1 : ""
     }
-    return phoneNumber;
+
+    const len = (String(number)).length;
+    if (len >= counter) {
+        let phoneNumber = "";
+        let n = len-1;
+        for(let i = 0; i < template.length; i++) {
+            if(template[i] === "#") {
+                phoneNumber += String(Math.floor(number / 10**n) % 10);
+                n -= 1;
+            } else {
+                phoneNumber = phoneNumber + template[i];
+            }
+        }
+        return phoneNumber;
+    } else {
+        return "Invalid phone number"
+    }
 }
 
-const result = formatNumber(972747271127, "+### (0##) ### ####")
+const result = formatNumber(9720747271127, "+### (###) ### ####")
 console.log(result);
+
+const formNumber = document.querySelector('.form-number');
+const inputNumber = document.querySelector('.input-number');
+const submitButton = document.querySelector('.submit-button');
+const myNumber = document.querySelector('.my-number');
+
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault()
+    myNumber.textContent = formatNumber(inputNumber.value, "+### (###) ### ####")
+})
