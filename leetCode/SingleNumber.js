a4b55cae3cb53ff0fc101e75a139e6db6061e3b2
Nitzan Cohen <nitzancohen@hotmail.com>
2023-02-25 17:56:44 +0200

    Exercise Single Number

---
+++ leetCode/SingleNumber.js
@@ -0,0 +1,26 @@
+/**
+ * Given a non-empty array of integers nums, every element appears
+ * twice except for one. Find that single one.
+ * You must implement a solution with a linear runtime complexity
+ * and use only constant extra space.
+ *
+ * @param {number[]} nums
+ * @return {number}
+ */
+console.clear();
+
+const singleNumber = function (nums) {
+  let singleNum = [nums[0]];
+  for (let i = 1; i < nums.length; i++) {
+    if (!singleNum.includes(nums[i])) {
+      singleNum = [...singleNum, nums[i] ]
+    } else {
+      singleNum = singleNum.filter(n => n !== nums[i])
+    }
+  }
+  return singleNum[0]
+};
+
+console.log(
+  singleNumber([1, 2, 3, 2, 1])
+);
