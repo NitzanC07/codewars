95e9e5eaa9218a93969c1581131c9a37db76e49e
Nitzan Cohen <nitzancohen@hotmail.com>
2023-02-28 10:55:15 +0200

    Exercise Searching insert position - leet code

--- leetCode/SearchInsertPosition.js
+++ leetCode/SearchInsertPosition.js
@@ -32,4 +32,6 @@ function searchInsert(nums, target) {
     return nums.length;
 };
 
-console.log(searchInsert([1, 3, 5, 6], 7)) // 2
+console.log(searchInsert([1, 3, 5, 6], 5)) // 2
+console.log(searchInsert([1, 3, 5, 6], 2)) // 1
+console.log(searchInsert([1, 3, 5, 6], 7)) // 4
