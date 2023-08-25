74994ebdf16924df69854e83f08cac90603a9226
Nitzan Cohen <nitzancohen@hotmail.com>
2023-08-25 13:04:54 +0300

    Exercise: Masked number

--- codeWars/Kata_6/MaskedNumber.js
+++ codeWars/Kata_6/MaskedNumber.js
@@ -1,4 +1,4 @@
-/** Simple Fun #258: Is divisible By 6 
+/**  Simple Fun #258: Is divisible By 6
  * 
  * A masked number is a string that consists of digits and one asterisk (*) that should be 
  * replaced by exactly one digit. Given a masked number s, find all the possible options to 
@@ -26,3 +26,25 @@
  * As you can see, the masked number may be very large ;-)
 */
 
+function isDivisibleBy6(s) {
+    let results = [];
+    const digits = s.split('');
+    const i = digits.findIndex(e => e === "*");
+    for (let n = 0; n <= 9; n++) {
+        digits[i] = n;
+        digits.map((n, l) => { digits[l] = Number(n) })
+        let a = 0;
+        for (let n = 0; n < digits.length; n++) {
+            a += digits[n] * (10**(digits.length -n-1));
+        }
+        if (a % 6 === 0) {
+            results = [...results, a.toString()]
+        }
+    }
+    
+    return results;
+}
+const result = isDivisibleBy6(`2*10`);
+const result2 = isDivisibleBy6(`${10**14}*0`);
+
+console.log(result);
